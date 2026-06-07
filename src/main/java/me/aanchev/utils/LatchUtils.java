67937eca0be3ba7ae54b4ac9f34e4b60ee41998f
Anko Anchev <ankos2@hotmail.com>
2026-06-07 14:39:08 +0100

    change the default await timeout to 10 hours

--- src/main/java/me/aanchev/utils/LatchUtils.java
+++ src/main/java/me/aanchev/utils/LatchUtils.java
@@ -1,11 +1,12 @@
 package me.aanchev.utils;
 
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 
 public class LatchUtils {
     public static void await(CountDownLatch latch) {
         try {
-            if (!latch.await(10, java.util.concurrent.TimeUnit.MINUTES)) {
+            if (!latch.await(10, TimeUnit.HOURS)) {
                 throw new IllegalStateException("Timed out");
             }
         } catch (Exception e) {
